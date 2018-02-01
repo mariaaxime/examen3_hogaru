cd2f7aeaecb92d47b60fa4f78d59dde1d4a0b837
Maria Acuna <mariaximenaam@gmail.com>
2018-02-01 22:26:37 +0000

    Adds style to views

--- app/models/calories_registry.rb
+++ app/models/calories_registry.rb
@@ -1,9 +1,16 @@
 class CaloriesRegistry < ApplicationRecord
+  after_create :increase_user_count
   belongs_to :user
   
   validates :quantity, presence: true
   validates :day, presence: true
   validates :registry_type, presence: true
   validates :comment, presence: true, length: { maximum: 150 }
+  
+  private
+  
+    def increase_user_count
+      self.user.increase_registries_count
+    end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   has_many :calories_registries
+  
+  attr_accessor :registries_count
+  
+  def increase_registries_count
+    registries_count ||= 0
+    registries_count = registries_count + 1
+  end
 end
